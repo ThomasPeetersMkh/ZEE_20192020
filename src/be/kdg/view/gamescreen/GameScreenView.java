6a793d829d4858d5747909b1eb17e89b031b1040
Emiel <emiel.kerckhofs@student.kdg.be>
2020-03-10 13:36:49 +0100

    Nieuwe

---
+++ src/be/kdg/view/gamescreen/GameScreenView.java
@@ -0,0 +1,26 @@
+package be.kdg.view.gamescreen;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+public class GameScreenView extends BorderPane {
+    private Computerscherm computerscherm;
+    private Spelerscherm spelerscherm;
+    public GameScreenView() {
+        this.initialiseNodes();
+        this.layoutNodes();
+    }
+
+    private void initialiseNodes() {
+
+    }
+
+    private void layoutNodes() {
+        // layout van de nodes
+
+    }
+
+    // implementatie van de nodige package-private Getters
+
+}
+
